eefe7ac6342343c7586a92896e02b93a86a52e15
olyayayaya <olga.zymyna@mail.ru>
2025-08-28 16:57:22 +0300

    changed

--- 3-struct/storage/storage.go
+++ 3-struct/storage/storage.go
@@ -10,17 +10,44 @@ import (
 )
 
 
-func (bins *bins.Bin) WriteFile() (content []byte, name string) {
+func WriteFile(content *bins.BinList, name string) {
+jsonData, err := json.Marshal(content)
+if err != nil {
+	fmt.Println(err)
+}
+
  file, err := os.Create(name)
  if err != nil {
 	fmt.Println(err)
  }
-_, err = file.Write(content)
+
+_, err = file.Write(jsonData)
  if err != nil {
 	file.Close()
 	fmt.Println(err)
 	return
  }
  	fmt.Println("запись успешнa")
-	return
+}
+
+
+func ReadFile(name string) *bins.BinList {
+		file, err := file.ReadFile(name)
+	if err != nil {	
+		return &bins.BinList {
+		Bins: []*bins.Bin{}, 
+		UpdatedAt: time.Now(),
+	}
+}
+
+var binlist bins.BinList 
+err = json.Unmarshal(file, &binlist)
+	if err != nil {	
+		fmt.Println(err.Error())
+		return &bins.BinList {
+		Bins: []*bins.Bin{}, 
+		UpdatedAt: time.Now(),
+	}
+	}
+return &binlist
 }
